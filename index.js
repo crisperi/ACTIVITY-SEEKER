be7808a323998f7cff521f67320c5f7c5d251304
crisperi <phoenixedian@gmail.com>
2024-04-11 18:33:58 +0300

    added domcontent loaded to js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+toggleDarkMode(light-mode);
+})
+
+//function to give dark mode class toogle 
 function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle("dark-mode");
